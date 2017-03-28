13b06e932cf5383eace0c4ff8db68578e64f4e46
andyiac <andyiaczhang@gmail.com>
2017-03-28 13:11:15 +0800

    modify build

--- build.sh
+++ build.sh
@@ -7,8 +7,17 @@ rm productor/index.html
 echo 'clean done'
 
 echo 'build start'
+
+cat src/index-header.html >> developer/index.html
 md2html docs/developer.md >> developer/index.html
+cat src/index-footer.html >> developer/index.html
+
+cat src/index-header.html >> designer/index.html
 md2html docs/designer.md >> designer/index.html
+cat src/index-footer.html >> designer/index.html
+
+cat src/index-header.html >> productor/index.html
 md2html docs/productor.md >> productor/index.html
+cat src/index-footer.html >> productor/index.html
 
 echo 'build end'
